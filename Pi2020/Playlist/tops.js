ef03d9dd7916a43ebb6f3cbcc9697ffe5b41851a
francoleone23 <63105854+francoleone23@users.noreply.github.com>
2020-06-17 16:16:19 -0300

    Create tops.js

---
+++ Pi2020/Playlist/tops.js
@@ -0,0 +1,39 @@
+// Obtener tops para homepage
+
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let url = proxy + "https://api.deezer.com/chart/0/";
+
+
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        
+        let artist = document.querySelector('.artists');
+        let album = document.querySelector('.album');
+        let tracks = document.querySelector('.tracks');
+        let resultadoArtista = datos.artists.data;
+        let resultadoAlbum = datos.album.data;
+        let resultadoTrack = datos.track.data;
+
+        resultadoTrack.forEach(function(track){
+            tracks.innerHTML += '<li>' + '<a href="track.html?id='+ track.id+ '">'+ track.title + '</a></li>' 
+        })
+
+        resultadoArtista.forEach(function(artist){
+            artist.innerHTML += '<li>' + '<a href="track.html?id='+ artist.id+ '">'+ artist.name + '</a></li>' 
+        })
+
+        resultadoAlbums.forEach(function(album){
+            album.innerHTML += '<li>' + '<a href="track.html?id='+ album.id+ '">'+ album.title + '</a></li>' 
+        })
+
+    })
+    .catch(function(error){
+        console.log(error);
+        
+    })
+
+
