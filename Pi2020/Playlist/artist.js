06e79bbca56edcc33ff9903a5f030d88a50bafb1
alejandrodh <alejandro@digitalhouse.com>
2020-06-19 07:57:41 -0300

    fixes de estilos en home, album, artist, playlist y track.

--- Pi2020/Playlist/artist.js
+++ Pi2020/Playlist/artist.js
@@ -20,8 +20,8 @@ fetch(url)
     tipo.style.textTransform = "uppercase";
     let seguidoresA = document.querySelector(".seguidoresA");
     seguidoresA.innerHTML = "<h4>Seguidores: "+data.nb_fan+"</h4>";
-    let fondo = document.querySelector(".main");
-    fondo.style.backgroundImage = "url("+data.picture_xl+ ")";
+    let fondo = document.querySelector(".albumcito");
+    fondo.src = data.picture_medium;
 })
 let urlt =proxi+ "https://api.deezer.com/artist/"+idartist+"/top?limit=5";
 console.log(urlt);
@@ -43,18 +43,18 @@ fetch(urlt)
 
 let urla =proxi+ "https://api.deezer.com/artist/"+idartist+"/albums?limit=3";
 
-fetch(urla)
-.then(function(response){
-    return response.json();
-})
-.then(function(datos){
-    let respuesta = datos.data
-    console.log(respuesta);
-    let albumcitos = document.querySelector(".albumes");
-    console.log();
+// fetch(urla)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(datos){
+//     let respuesta = datos.data
+//     console.log(respuesta);
+//     let albumcitos = document.querySelector(".albumes");
+//     console.log();
     
-    respuesta.forEach(function(albumes){
-        albumcitos.innerHTML += "<div class='albumx'><a href='./album.html?id="+albumes.id+"'><img src='"+albumes.cover_medium+"' alt='foto'><br><h7>"+albumes.title+"</h7></a></div>";
+//     respuesta.forEach(function(albumes){
+//         albumcitos.innerHTML += "<div class='albumx'><a href='./album.html?id="+albumes.id+"'><img src='"+albumes.cover_medium+"' alt='foto'><br><h7>"+albumes.title+"</h7></a></div>";
         
-    })
-})
+//     })
+// })
